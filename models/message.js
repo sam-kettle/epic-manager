dd77809fe5ae6d5f942b472d11ea4504ebcfed7c
Sam Eddy <sam-kettle@outlook.com>
2020-06-27 23:07:05 +0100

    Message board displays messages from db

---
+++ models/message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// Define Schema
+let messageSchema = mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+// Static methods
+messageSchema.statics.returnAll = function() {
+    return mongoose.model('Message').find()
+}
+
+let Message = module.exports = mongoose.model('Message', messageSchema)
--- routes/message-board.js
+++ routes/message-board.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router();
+const Message = require('../models/message')
 
 
 router.get('/', (req, res) => {
-    res.render('pages/message-board', { 
-        headertitle: "Message Board || Epic Manager"
+    Message.returnAll().then((messages) => {
+        res.render('pages/message-board', { 
+            headertitle: "Message Board || Epic Manager",
+            messages: messages,
+        })
     })
 })
 
 router.get('/add', (req, res) => {
+
     res.render('pages/add-message', { 
         headertitle: "Add Message || EPIC Manager"
     })
