ac57877f3b2bfb9493ac1b0c4ab1ae8c87ac2cca
Sam Eddy <sam-kettle@outlook.com>
2020-06-30 18:11:50 +0100

    Create Mongoose model for reviews

---
+++ models/review.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// Define Schema
+let reviewSchema = mongoose.Schema({
+    employee: {
+        type: String,
+        required: true
+    },
+    reviewer: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    objectives: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+    
+})
+
+// Static methods
+reviewSchema.statics.returnAll = function() {
+    return mongoose.model('Review').find()
+}
+
+let Review = module.exports = mongoose.model('Review', reviewSchema)
