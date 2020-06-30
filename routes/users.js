cc53a90626615e749e4d6ef786f031fedb84667b
Sam Eddy <sam-kettle@outlook.com>
2020-06-30 20:43:18 +0100

    Simple register form validation and alerts

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,32 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('pages/register', {
         headertitle: 'Register || EPIC Manager', 
+        errors: undefined
     })
 })
 
 router.post('/register', (req, res) => {
-    console.log(req.body)
+    const { name, email, password, password2, isManager } = req.body
+    
+    // Simple validation
+    let errors = []
+    if (!name || !email || !password || !password2 || !isManager) {
+        errors.push({ msg: 'You must complete all fields to register' })
+    }
+    if (password !== password2) {
+        errors.push({ msg: 'Your passwords must match' })
+    }
+    if (password.length < 8) {
+        errors.push({ msg: 'Password must be at least 8 characters long' })
+    }
+    if (errors.length > 0) {
+        res.render('pages/register', {
+            headertitle: 'Register || EPIC Manager',
+            errors: errors
+        })
+    } else {
+        res.send('Success')
+    }
 })
 
 module.exports = router
